1d640ebbfc9b7abf76f2216a9334bb4816ab49d0
developer-harshil-gupta <harshil.gupta_cs19@gla.ac.in>
2020-08-30 18:25:55 +0530

    add fields and component to Book.java

--- src/definitions/Book.java
+++ src/definitions/Book.java
@@ -6,5 +6,64 @@
  * */
 package definitions;
 
+
+import java.util.Objects;
+
 public class Book {
+    private String bookName;
+    private String authorName;
+    private String isbnNumber;
+
+    public String getBookName() {
+        return bookName;
+    }
+
+    public String getAuthorName() {
+        return authorName;
+    }
+
+    public String getIsbnNumber() {
+        return isbnNumber;
+    }
+
+    public void setBookName(String bookName) {
+        this.bookName = bookName;
+    }
+
+    public void setAuthorName(String authorName) {
+        this.authorName = authorName;
+    }
+
+    public void setIsbnNumber(String isbnNumber) {
+        this.isbnNumber = isbnNumber;
+    }
+    public Book(){
+        this.bookName= getBookName();
+        this.authorName=getAuthorName();
+        this.isbnNumber=getIsbnNumber();
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "bookName='" + bookName + '\'' +
+                ", authorName='" + authorName + '\'' +
+                ", isbnNumber='" + isbnNumber + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return Objects.equals(getBookName(), book.getBookName()) &&
+                Objects.equals(getAuthorName(), book.getAuthorName()) &&
+                Objects.equals(getIsbnNumber(), book.getIsbnNumber());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getBookName(), getAuthorName(), getIsbnNumber());
+    }
 }
