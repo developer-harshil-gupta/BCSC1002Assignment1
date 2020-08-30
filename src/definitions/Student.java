e3173557b94fea5b85d06c0fa47fbba789307280
developer-harshil-gupta <harshil.gupta_cs19@gla.ac.in>
2020-08-30 21:43:31 +0530

    add getter methods for fields in Student.java

--- src/definitions/Student.java
+++ src/definitions/Student.java
@@ -7,4 +7,27 @@
 package definitions;
 
 public class Student {
+    private final int totalTypesOfNamesOfAPerson = 3;
+    private String [] studentName = new String[totalTypesOfNamesOfAPerson];
+    private long universityRollNumber;
+    private int numberOfBooksIssued;
+    private String[] bookNames;
+
+    public String[] getStudentName() {
+        return studentName;
+    }
+
+    public long getUniversityRollNumber() {
+        return universityRollNumber;
+    }
+
+    public int getNumberOfBooksIssued() {
+        return numberOfBooksIssued;
+    }
+
+    public String[] getBookNames() {
+        return bookNames;
+    }
+
 }
+
